e656170bd9b3c508f0ef225627e2ee97a0fc21f7
Yuki Akiyama <sasanoha.payapaya@gmail.com>
2020-02-10 19:06:10 +0900

    Add files via upload

---
+++ farmer.js
@@ -0,0 +1,22 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log(`Botを起動しました`);
+});
+
+client.on('message', message => {
+    if (message.content === "パンダ") {
+    message.reply("パンダじゃねえ！");
+  }
+    if (message.content === "farmer") {
+    message.reply("呼んだ？");
+  }
+    
+   
+});
+
+client.login("xxxxxxx");
+
+
+
